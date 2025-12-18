a9228f2805c2a2b41f3a857e60c9dfd641a5049a
mehalforc <sdberndt@charter.net>
2025-12-17 22:32:17 -0600

    Add files via upload

---
+++ Iswell/GPS/Locales/de/TranslatedGPS.lua
@@ -0,0 +1,13 @@
+--[[
+    de Locale for GPS Plugin...
+]]
+
+_G.T = {
+  MAIN_WELCOME = "Willkommen bei GPS!!!\nBitte wählen Sie eine Kategorie aus...",
+  SELECTED_CATEGORY = "Ausgewählt: %s (Kategorie %d)",
+  NO_CATEGORY_SELECTED = "Keine Kategorie ausgewählt...",
+  CLOSEST_COORDINATES = "Nächstgelegener Punkt: %s\nKoordinaten: (%.5f, %.5f)\nEntfernung: ~%.5fm\nZeilennummer: %d",
+  NO_POINTS_MATCHING = "Keine passenden Punkte für Kategorie: %s, in Region: %s",
+  SELECT_CATEGORY = "Bitte wählen Sie eine Kategorie!",
+  GO_OUTSIDE = "Sie müssen draußen sein, um diese Funktion zu nutzen.",
+}
---
+++ Iswell/GPS/Locales/fr/TranslatedGPS.lua
@@ -0,0 +1,13 @@
+--[[
+    fr Locale for GPS Plugin...
+]]
+
+_G.T = {
+  MAIN_WELCOME = "Bienvenue dans GPS!!!\nVeuillez sélectionner une catégorie ci-dessus...",
+  SELECTED_CATEGORY = "Sélectionné: %s (Catégorie %d)",
+  NO_CATEGORY_SELECTED = "Aucune catégorie sélectionnée...",
+  CLOSEST_COORDINATES = "Point le plus proche: %s\nCoordonnées: (%.5f, %.5f)\nDistance: ~%.5fm\nNuméro de ligne: %d",
+  NO_POINTS_MATCHING = "Aucun point correspondant pour la catégorie: %s, dans la région: %s",
+  SELECT_CATEGORY = "Veuillez sélectionner une catégorie!",
+  GO_OUTSIDE = "Vous devez être à l'extérieur pour utiliser cette fonctionnalité.",
+}
